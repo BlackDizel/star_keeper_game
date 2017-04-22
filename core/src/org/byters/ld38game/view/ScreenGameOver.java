015a31c79308cb111a17f11ca257a7b2eead2e50
blackdizel <blackdizel@gmail.com>
2017-04-22 21:11:21 +0300

    add game over screen stub

---
+++ core/src/org/byters/ld38game/view/ScreenGameOver.java
@@ -0,0 +1,31 @@
+package org.byters.ld38game.view;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import org.byters.engine.view.IScreen;
+
+public class ScreenGameOver implements IScreen {
+    @Override
+    public void draw(SpriteBatch batch) {
+        //todo implement text "Your flower is die, brave hero"
+    }
+
+    @Override
+    public void load(SpriteBatch batch) {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void input() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
