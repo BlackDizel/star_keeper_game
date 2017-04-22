751f97c60ecc382f506d49187878b638347d3f85
blackdizel <blackdizel@gmail.com>
2017-04-22 22:22:34 +0300

    add game over text

--- core/src/org/byters/ld38game/view/ScreenGameOver.java
+++ core/src/org/byters/ld38game/view/ScreenGameOver.java
@@ -1,17 +1,28 @@
 package org.byters.ld38game.view;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import org.byters.engine.view.IScreen;
 
 public class ScreenGameOver implements IScreen {
+
+    BitmapFont font;
+    GlyphLayout layout;
+
     @Override
     public void draw(SpriteBatch batch) {
-        //todo implement text "Your flower is die, brave hero"
+        font.draw(batch, layout
+                , (Gdx.graphics.getWidth() - layout.width) / 2, Gdx.graphics.getHeight() / 2);
     }
 
     @Override
     public void load(SpriteBatch batch) {
+        font = new BitmapFont();
 
+        layout = new GlyphLayout();
+        layout.setText(font, "Your flower is die, brave hero");
     }
 
     @Override
