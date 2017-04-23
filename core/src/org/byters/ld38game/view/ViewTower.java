a31cb7aca8dcc8a0734745226166c06fae26014e
blackdizel <blackdizel@gmail.com>
2017-04-23 18:18:54 +0300

    add towers

--- core/src/org/byters/ld38game/view/ViewTower.java
+++ core/src/org/byters/ld38game/view/ViewTower.java
@@ -8,37 +8,55 @@ import org.byters.ld38game.controller.ControllerPlayer;
 
 
 class ViewTower {
-    private static final int DELTA_POSITION = 3;
-    private static final String TEXTURE_TOWER = "textures/tower_main.png";
-    private Texture tTower;
-    private float pos;
+    private static final String TEXTURE_TOWER_MAIN = "textures/tower_main.png";
+    private static final String TEXTURE_TOWER = "textures/tower.png";
+    private Texture tTowerMain, tTower;
+    private float posMain, pos1, pos2;
 
     void draw(SpriteBatch batch) {
-        batch.draw(tTower, pos, tTower.getHeight() * ControllerGameFlow.getInstance().getScale() / 12f
+        batch.draw(tTowerMain, posMain, tTowerMain.getHeight() * ControllerGameFlow.getInstance().getScale() / 12f
+                , tTowerMain.getWidth() * ControllerGameFlow.getInstance().getScale(), tTowerMain.getHeight() * ControllerGameFlow.getInstance().getScale());
+
+        batch.draw(tTower, pos1, tTower.getHeight() * ControllerGameFlow.getInstance().getScale() / 12f
                 , tTower.getWidth() * ControllerGameFlow.getInstance().getScale(), tTower.getHeight() * ControllerGameFlow.getInstance().getScale());
+
+        batch.draw(tTower, pos2, tTower.getHeight() * ControllerGameFlow.getInstance().getScale() / 12f
+                , tTower.getWidth() * ControllerGameFlow.getInstance().getScale(), tTower.getHeight() * ControllerGameFlow.getInstance().getScale());
+
     }
 
     void load() {
+        tTowerMain = new Texture(Gdx.files.internal(TEXTURE_TOWER_MAIN));
         tTower = new Texture(Gdx.files.internal(TEXTURE_TOWER));
 
-        pos = Gdx.graphics.getWidth() / 3f * 2;
+        posMain = Gdx.graphics.getWidth() / 3f * 2;
+        pos1 = Gdx.graphics.getWidth() / 3f * 4;
+        pos2 = 0;
     }
 
     void dispose() {
+        tTowerMain.dispose();
         tTower.dispose();
     }
 
     void update() {
         if (ControllerPlayer.getInstance().isBounded()) {
             float deltaX = ControllerPlayer.getInstance().getDelta();
-            pos -= deltaX;
+            posMain -= deltaX;
+            pos1 -= deltaX;
+            pos2 -= deltaX;
 
-            if (pos < -getDeltaPosition()) pos = Gdx.graphics.getWidth() + getDeltaPosition();
-            if (pos > Gdx.graphics.getWidth() + getDeltaPosition()) pos = -getDeltaPosition();
+            posMain = checkPos(posMain);
+            pos1 = checkPos(pos1);
+            pos2 = checkPos(pos2);
         }
     }
 
-    private int getDeltaPosition() {
-        return Gdx.graphics.getWidth() / DELTA_POSITION;
+    private float checkPos(float pos) {
+        if (pos < -Gdx.graphics.getWidth() / 2) return Gdx.graphics.getWidth() + Gdx.graphics.getWidth() / 2;
+        if (pos > Gdx.graphics.getWidth() + Gdx.graphics.getWidth() / 2) return -Gdx.graphics.getWidth() / 2;
+
+        return pos;
     }
+
 }
