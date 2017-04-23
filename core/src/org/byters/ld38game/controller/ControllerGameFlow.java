b3381038a76c340af271bec39fdc8fa1613230fe
blackdizel <blackdizel@gmail.com>
2017-04-23 13:27:08 +0300

    add screen win navigation, add method get scale

--- core/src/org/byters/ld38game/controller/ControllerGameFlow.java
+++ core/src/org/byters/ld38game/controller/ControllerGameFlow.java
@@ -2,6 +2,7 @@ package org.byters.ld38game.controller;
 
 import org.byters.engine.controller.ControllerMain;
 import org.byters.ld38game.view.ScreenGameOver;
+import org.byters.ld38game.view.ScreenWin;
 
 public class ControllerGameFlow {
     private static ControllerGameFlow instance;
@@ -15,10 +16,19 @@ public class ControllerGameFlow {
 
         //todo implement enemies waves
 
+        if (isWin()) ControllerMain.getInstance().navigateScreen(new ScreenWin());
         if (isGameOver()) ControllerMain.getInstance().navigateScreen(new ScreenGameOver());
     }
 
     private boolean isGameOver() {
         return ControllerFlower.getInstance().isDie();
+    }
+
+    private boolean isWin() {
+        return ControllerFlower.getInstance().isGrown();
+    }
+
+    public float getScale() {
+        return 2;
     }
 }
