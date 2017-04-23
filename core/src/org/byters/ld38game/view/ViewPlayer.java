5ec3fd55fb49fff700db9561fbfce758a6c2ed99
blackdizel <blackdizel@gmail.com>
2017-04-23 13:27:59 +0300

    add player animation

--- core/src/org/byters/ld38game/view/ViewPlayer.java
+++ core/src/org/byters/ld38game/view/ViewPlayer.java
@@ -2,21 +2,62 @@ package org.byters.ld38game.view;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import org.byters.engine.controller.ControllerMain;
+import org.byters.ld38game.controller.ControllerGameFlow;
 import org.byters.ld38game.controller.ControllerPlayer;
 
 class ViewPlayer {
 
-    private static final String TEXTURE_PLAYER = "textures/player.png";
+    private static final float FRAME_DURATION_SECONDS = 0.09f;
+    private static final int FRAMES_NUM_MOVE = 4;
+
+    private static final String TEXTURE_PLAYER = "textures/spr_starkeep.png";
     private Texture tPlayer;
+
+    private Animation<TextureRegion> aPlayer;
 
     void load() {
         tPlayer = new Texture(Gdx.files.internal(TEXTURE_PLAYER));
+
+        int frameWidth = tPlayer.getWidth() / FRAMES_NUM_MOVE;
+        int frameHeight = tPlayer.getHeight();
+        TextureRegion[][] frames = TextureRegion.split(tPlayer, frameWidth, frameHeight);
+        TextureRegion[] framesMove = new TextureRegion[FRAMES_NUM_MOVE];
+        for (int i = 0; i < FRAMES_NUM_MOVE; ++i)
+            framesMove[i] = frames[0][i];
+
+        aPlayer = new Animation<TextureRegion>(FRAME_DURATION_SECONDS, framesMove);
+        aPlayer.setPlayMode(Animation.PlayMode.LOOP);
     }
 
     void draw(SpriteBatch batch) {
-        batch.draw(tPlayer, ControllerPlayer.getInstance().getPositionX(), ControllerPlayer.getInstance().getPositionY());
+        TextureRegion texture = getCurrentFrame();
+        batch.draw(texture,
+                ControllerPlayer.getInstance().getPositionX(), ControllerPlayer.getInstance().getPositionY()
+                , 0, 0,
+                texture.getRegionWidth(), texture.getRegionHeight()
+                , ControllerGameFlow.getInstance().getScale(), ControllerGameFlow.getInstance().getScale(),
+                0
+        );
+    }
+
+
+    private void checkFlip(TextureRegion textureRegion) {
+        if (!ControllerPlayer.getInstance().isPlayerDirectionRight() && !textureRegion.isFlipX()
+                || ControllerPlayer.getInstance().isPlayerDirectionRight() && textureRegion.isFlipX())
+            textureRegion.flip(true, false);
+    }
+
+    private TextureRegion getCurrentFrame() {
+        TextureRegion result;
+        if (ControllerPlayer.getInstance().getMoveDirection() == ControllerPlayer.DIRECTION_NONE)
+            result = aPlayer.getKeyFrame(0);
+        else result = aPlayer.getKeyFrame(ControllerMain.getInstance().getGameTime() / 1000f);
+        checkFlip(result);
+        return result;
     }
 
     void dispose() {
