34a20e817d89522bb5667c77b006518a60a12aba
blackdizel <blackdizel@gmail.com>
2017-04-24 22:37:44 +0300

    add star wave

--- core/src/org/byters/ld38game/model/StarInfo.java
+++ core/src/org/byters/ld38game/model/StarInfo.java
@@ -1,17 +1,22 @@
 package org.byters.ld38game.model;
 
 
+import org.byters.engine.controller.ControllerMain;
 import org.byters.engine.model.PointInt;
 
 public class StarInfo {
+    private static final long TIME_CHANGE_DIRECTION_MILLIS = 2000;
     private static float originX;
     private static float originY;
     private float positionX;
     private float positionY;
+    private boolean isRightWave;
 
     public StarInfo(float positionX, float positionY) {
         this.positionX = positionX;
         this.positionY = positionY;
+
+        isRightWave = ControllerMain.getInstance().getRandom().nextBoolean();
     }
 
     public static void setOrigin(float originX, float originY) {
@@ -20,7 +25,19 @@ public class StarInfo {
     }
 
     public float getPositionX() {
-        return positionX;
+        return positionX + getDelta();
+    }
+
+    private int getDelta() {
+        long check = ControllerMain.getInstance().getGameTime() % TIME_CHANGE_DIRECTION_MILLIS;
+        int factor = check < TIME_CHANGE_DIRECTION_MILLIS / 4
+                ? 0
+                : check < TIME_CHANGE_DIRECTION_MILLIS / 2
+                ? -1
+                : check < TIME_CHANGE_DIRECTION_MILLIS / 4 * 3
+                ? 0
+                : 1;
+        return isRightWave ? factor : factor * -1;
     }
 
     public float getPositionY() {
